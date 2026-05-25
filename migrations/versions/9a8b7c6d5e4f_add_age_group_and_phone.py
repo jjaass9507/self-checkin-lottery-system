4474bbcf1276655c3f04767b2ed053eeb8590306
jjaass9507 <jjaass9507@gmail.com>
2026-05-25 15:17:48 +0800

    Add migration for age group and phone

---
+++ migrations/versions/9a8b7c6d5e4f_add_age_group_and_phone.py
@@ -0,0 +1,28 @@
+"""add age_group and phone
+
+Revision ID: 9a8b7c6d5e4f
+Revises: 651c6c783d29
+Create Date: 2026-05-25 14:30:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9a8b7c6d5e4f'
+down_revision = '651c6c783d29'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table('checkin_list', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('age_group', sa.String(length=20), nullable=True))
+        batch_op.add_column(sa.Column('phone', sa.String(length=30), nullable=True))
+
+
+def downgrade():
+    with op.batch_alter_table('checkin_list', schema=None) as batch_op:
+        batch_op.drop_column('phone')
+        batch_op.drop_column('age_group')
